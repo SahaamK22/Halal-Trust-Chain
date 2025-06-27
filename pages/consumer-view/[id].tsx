ce8a59613c567e011945e2547756e5adc551c257
Sahaam Khalil <kashifnadeem@Sahaams-MacBook-Air.local>
2025-06-28 03:51:55 +0500

    Fix deploy test - minor change to [id].tsx

--- pages/consumer-view/[id].tsx
+++ pages/consumer-view/[id].tsx
@@ -6,7 +6,8 @@ export default function ConsumerViewPage() {
 
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Consumer View</h1>
+      <h1 className="text-2xl font-bold mb-4">Consumer View ✅</h1>
+
       <p className="text-lg">Animal ID: <strong>{id}</strong></p>
     </div>
   );
