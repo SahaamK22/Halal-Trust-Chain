7e3d0bd717e38b8b209634d9ebefa0c2d3b5bb65
Sahaam Khalil <kashifnadeem@Sahaams-MacBook-Air.local>
2025-07-01 19:18:38 +0500

    Moved frontend files to root and finalized structure for live deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
   },
 };
 
